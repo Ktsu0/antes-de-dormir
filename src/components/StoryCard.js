4b56d3cfb9d7665875f005af4854e87577804ddc
Ktsu0 <gabrielwag971@gmail.com>
2026-02-10 20:28:55 -0300

    V.0.0.1.6 - Correções de Erros

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -1,4 +1,4 @@
-import React, { useState, memo, useEffect } from "react";
+import React, { useState, memo } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import {
   Heart,
